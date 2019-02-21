d34cc862a1ee3041c1b8f494a376160e1aca44d7
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-20 19:19:03 -0600

    made the list of studnets grades more dry

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
 
-	const int LEN = 2;
+	const int MAX = 30;
+	int LEN;
+	
+	int student[MAX];
+	string name[MAX];
+	
+	cout <<" How many students are in this class: ";
+	cin >> LEN;
+
 	
 	int students[LEN];
+	string name[LEN] = {"Bob", "Jill"};
 	
 	for(int i=0; i<LEN; i++){
-		cout << "enter student, " << i+1 << "'s grade ";
+		cout << "Enter student, " << i+1 << "'s name ";
+		cin >> name[i];
+		cout << " Enter " << name[i] << "'s grade: ";
 		cin >> students[i];
 	}
 	
 		for(int s=0; s<LEN; s++){
-			cout << "student  " << s+1 << "'s grade is " << students[s] << endl;
+			cout << name[s] << "'s grade is " << students[s] << endl;
 	}
 	
 	return 0;
